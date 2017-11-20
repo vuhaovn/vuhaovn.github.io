abd83d615085067e3a7fae204f0272b6d721dc67
Tran Vu Hao <theflash8888@gmail.com>
2017-11-20 19:00:49 +0700

    add portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require("gulp");
+var sass = require("gulp-sass");
 var browserSync = require("browser-sync");
+var livereload = require("gulp-livereload");
 var reload = browserSync.reload;
 
 gulp.task("serve", [], function() {
@@ -9,8 +11,21 @@ gulp.task("serve", [], function() {
 			baseDir: '.'
 		}
 	});
-	gulp.watch(['**/*.html'], reload);
-	gulp.watch(['**/js/*.js'], reload);
+	gulp.watch(["**/*.html"], reload);
+	gulp.watch(["**/js/*.js"], reload);
+	gulp.watch(["**/css/*.css"], reload);
 });
 
-gulp.task("default", ["serve"]);
+gulp.task("sass", function() {
+	return gulp.src("share/scss/style.scss")
+		.pipe(sass())
+		.pipe(gulp.dest("share/css"))
+		.pipe(livereload());
+});
+
+gulp.task("watch", function () {
+	livereload.listen();
+	gulp.watch(["share/scss/style.scss"], ["sass"]);
+});
+
+gulp.task("default", ["serve", "sass", "watch"]);
