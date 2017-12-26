93e039f86b69359667481dd346bef6e77cd6baa7
bakahao <theflash8888@gmail.com>
2017-12-26 11:43:34 +0700

    jstree

--- jquery-tree/js/index.js
+++ jquery-tree/js/index.js
@@ -1,129 +1,164 @@
-;(function($){
-	"use strict";
+(function($) {
+  "use strict";
 
-	var
-		padding = 15,
-		$item = $(".item");
+  function init() {
+    $('#tree')
+      .jstree({
+        'core': {
+          'data': {
+            'url': '?operation=get_node',
+            'data': function(node) {
+              return { 'id': node.id };
+            }
+          },
+          'check_callback': function(o, n, p, i, m) {
+            if (m && m.dnd && m.pos !== 'i') { return false; }
+            if (o === "move_node" || o === "copy_node") {
+              if (this.get_node(n).parent === this.get_node(p).id) { return false; }
+            }
+            return true;
+          },
+          'themes': {
+            'responsive': false,
+            'variant': 'small',
+            'stripes': true
+          }
+        },
+        'sort': function(a, b) {
+          return this.get_type(a) === this.get_type(b) ? (this.get_text(a) > this.get_text(b) ? 1 : -1) : (this.get_type(a) >= this.get_type(b) ? 1 : -1);
+        },
+        'contextmenu': {
+          'items': function(node) {
+            var tmp = $.jstree.defaults.contextmenu.items();
+            delete tmp.create.action;
+            tmp.create.label = "New";
+            tmp.create.submenu = {
+              "create_folder": {
+                "separator_after": true,
+                "label": "Folder",
+                "action": function(data) {
+                  var inst = $.jstree.reference(data.reference),
+                    obj = inst.get_node(data.reference);
+                  inst.create_node(obj, { type: "default" }, "last", function(new_node) {
+                    setTimeout(function() { inst.edit(new_node); }, 0);
+                  });
+                }
+              },
+              "create_file": {
+                "label": "File",
+                "action": function(data) {
+                  var inst = $.jstree.reference(data.reference),
+                    obj = inst.get_node(data.reference);
+                  inst.create_node(obj, { type: "file" }, "last", function(new_node) {
+                    setTimeout(function() { inst.edit(new_node); }, 0);
+                  });
+                }
+              }
+            };
+            if (this.get_type(node) === "file") {
+              delete tmp.create;
+            }
+            return tmp;
+          }
+        },
+        'types': {
+          'default': { 'icon': 'folder' },
+          'file': { 'valid_children': [], 'icon': 'file' }
+        },
+        'unique': {
+          'duplicate': function(name, counter) {
+            return name + ' ' + counter;
+          }
+        },
+        'plugins': ['state', 'dnd', 'sort', 'types', 'contextmenu', 'unique']
+      })
+      .on('delete_node.jstree', function(e, data) {
+        $.get('?operation=delete_node', { 'id': data.node.id })
+          .fail(function() {
+            data.instance.refresh();
+          });
+      })
+      .on('create_node.jstree', function(e, data) {
+        $.get('?operation=create_node', { 'type': data.node.type, 'id': data.node.parent, 'text': data.node.text })
+          .done(function(d) {
+            data.instance.set_id(data.node, d.id);
+          })
+          .fail(function() {
+            data.instance.refresh();
+          });
+      })
+      .on('rename_node.jstree', function(e, data) {
+        $.get('?operation=rename_node', { 'id': data.node.id, 'text': data.text })
+          .done(function(d) {
+            data.instance.set_id(data.node, d.id);
+          })
+          .fail(function() {
+            data.instance.refresh();
+          });
+      })
+      .on('move_node.jstree', function(e, data) {
+        $.get('?operation=move_node', { 'id': data.node.id, 'parent': data.parent })
+          .done(function(d) {
+            //data.instance.load_node(data.parent);
+            data.instance.refresh();
+          })
+          .fail(function() {
+            data.instance.refresh();
+          });
+      })
+      .on('copy_node.jstree', function(e, data) {
+        $.get('?operation=copy_node', { 'id': data.original.id, 'parent': data.parent })
+          .done(function(d) {
+            //data.instance.load_node(data.parent);
+            data.instance.refresh();
+          })
+          .fail(function() {
+            data.instance.refresh();
+          });
+      })
+      .on('changed.jstree', function(e, data) {
+        if (data && data.selected && data.selected.length) {
+          $.get('?operation=get_content&id=' + data.selected.join(':'), function(d) {
+            if (d && typeof d.type !== 'undefined') {
+              $('#data .content').hide();
+              switch (d.type) {
+                case 'text':
+                case 'txt':
+                case 'md':
+                case 'htaccess':
+                case 'log':
+                case 'sql':
+                case 'php':
+                case 'js':
+                case 'json':
+                case 'css':
+                case 'html':
+                  $('#data .code').show();
+                  $('#code').val(d.content);
+                  break;
+                case 'png':
+                case 'jpg':
+                case 'jpeg':
+                case 'bmp':
+                case 'gif':
+                  $('#data .image img').one('load', function() { $(this).css({ 'marginTop': '-' + $(this).height() / 2 + 'px', 'marginLeft': '-' + $(this).width() / 2 + 'px' }); }).attr('src', d.content);
+                  $('#data .image').show();
+                  break;
+                default:
+                  $('#data .default').html(d.content).show();
+                  break;
+              }
+            }
+          });
+        } else {
+          $('#data .content').hide();
+          $('#data .default').html('Select a file from the tree.').show();
+        }
+      });
+  }
 
-	function init() {
-		hover($item);
-		according($item);
-		addDom();
-	}
-
-	function hover(item) {
-		var
-			button = "<div class='btn_option'>&#9679;&#9679;&#9679;</div>";
-		item.on({
-			"mouseenter": function() {
-				$(this).append(button);
-			},
-			"mouseleave": function() {
-				$(this).find(".btn_option").remove();
-			}
-		});
-	}
-
-	function list($items) {
-		var 
-			foOpt = '<ul class="file_list">';
-			foOpt +='<li data-action="new_folder">New Folder</li>';
-			foOpt +='<li data-action="new_file">New File</li>';
-			foOpt +='<li data-action="rename">Rename</li>';
-			foOpt +='<li data-action="delete">Delete</li>';
-			foOpt +='</ul>';
-
-		var
-			fiOpt = '<ul class="file_list">';
-			fiOpt +='<li data-action="rename">Rename</li>';
-			fiOpt +='<li data-action="delete">Delete</li>';
-			fiOpt +='</ul>';
-
-		$(".btn_option").on("click", function() {
-			$items.addClass("active");
-			if ($items.data("type") == "folder") {
-				$items.append(foOpt);
-				addDom($items);
-			} else {
-				$items.append(fiOpt);
-			}
-		});
-
-		$(document).mouseup(function(e) {
-		  var container = $(".btn_option");
-		  if (!container.is(e.target) && container.has(e.target).length === 0) {
-				$items.find(".file_list").hide();
-				$items.removeClass("active");
-				setTimeout(function() {
-					$items.find(".file_list").remove();
-				}, 1000);
-		  }
-		});
-
-	}
-
-	function addDom() {
-		var item = "";
-		$("#addFolder").click(function() {
-			item += '<div class="sub_item">';
-			item += '<div class="item" data-type="folder"><span class="icon_close">css</span></div>';
-			item += '</div>';
-			$item.append(item);
-		});
-	}
-
-	function action($li, $items) {
-		// var
-		// 	folder = '<div class="sub_item">';
-		// 	folder += '<div class="item" data-type="folder"><span class="icon_close">css</span></div>';
-		// 	folder += '</div>';
-
-		// var
-		// 	file = '<div class="sub_item">';
-		// 	file += '<div class="item" data-type="file"><span class="icon_file">index.html</span></div>';
-		// 	file += '</div>';
-
-		$("input[name='txt_name']").keyup(function() {
-			if ($(this).val().length > 0) {
-				$("#submit").show();
-				$("#cancel").hide();
-			} else {
-				$("#cancel").show();
-				$("#submit").hide();
-			}
-		});
-
-		$("#submit").click(function() {
-			$items.after(folder);
-			$items.find(".form").remove();
-			console.log($.parseHTML(folder).find(".item"));
-			hover($items);
-			according($items);
-		});
-
-		$("#cancel").click(function() {
-			$this.find(".form").remove();
-		});
-	}
-
-	function according(item) {
-		item.next(".sub_item").css("padding-left", padding+"px");
-		item.click(function(e) {
-			var $ele = $("span");
-			if ($ele.is(e.target)) {
-				var isOpen = $(this).children("span").hasClass("open");
-				if (isOpen) {
-					$(this).children("span").removeClass("open");
-				} else if ($(this).next(".sub_item").length > 0) {
-					$(this).children("span").addClass("open");
-				}
-				$(this).next(".sub_item").stop().slideToggle(50);
-			}
-		});
-	}
-
-	$(function() {
-		init();
-	});
+  $(function() {
+    init();
+  });
 
 })(jQuery);
